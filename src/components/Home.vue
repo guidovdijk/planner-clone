4137843692ccff0cc433e10dff19a99a0eb8f213
Guido van Dijk <GuidoD@Guido-van-Dijk.local>
2019-06-30 17:27:52 +0200

    Added new styling and functionality

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,49 +3,64 @@
 		<TopNavbar />
 
 		<div class="columns">
-			<div class="column is-multiline is-12">
-				<form id="form"  v-on:submit.prevent="addTile">
+			<div class="column is-9">
+				<div class="table-wrapper">
+					<table class="table is-fullwidth">
+						<thead>
+							<tr v-for="(tile) in this.$store.getters.getTiles.slice(0,1)" v-bind:key="tile.id">
+								<th></th>
+								<th v-for="(value, name, index) in filteredTiles(tile)" v-bind:key="index">
+									{{name}}
+								</th>
+								<th>
+									<div :class="newColumn ? 'is-active' : ''" class="dropdown is-right">
+										<div class="dropdown-trigger">
+											<span class="icon dropdown-icon" @click="newColumn = !newColumn">
+												<i class="fas fa-plus-circle"></i>
+											</span>
+										</div>
+										<div class="dropdown-menu" id="dropdown-menu" role="menu">
+											<div class="dropdown-content">
+												<a href="#" class="dropdown-item">
+													Dropdown item
+												</a>
+											</div>
+										</div>
+									</div>
+								</th>
+							</tr>
+						</thead>
+						<tbody class="has-background-light">
+							<tr class="is-multiline" v-for="(tile) in this.$store.getters.getTiles" v-bind:key="tile.id">
+								
+								<td class="is-sticky">
+									<span class="icon" @click="deleteTile(tile.id)">
+										<i class="fas fa-caret-down"></i>
+									</span>
 
-					<div class="field is-grouped" >
-						<p class="control is-expanded" v-for="(value, name, index) in newTile" v-bind:key="index">
-							<label class="label">{{name}}</label>
-							<input 
-								type="text" 
-								class="input" 
-								v-model="newTile[name]"
-							/>					
+									<input v-on:change="updateTitle($event, tile)" type="text" :value="tile.title"/>
+								</td>
+
+								<td v-for="(value, name, index) in filteredTiles(tile)" v-bind:key="index">
+									<input v-on:change="updataData($event, name, tile.id)" type="text" :value="value"/>
+								</td>
+								
+							</tr>
+						</tbody>
+					</table>
+
+				</div>
+				<form v-on:submit.prevent="addTile">
+					<div class="field is-grouped">
+						<p class="control is-expanded">
+							<input v-model="newTile.title" class="input add-tile has-text-grey-light" placeholder="+ Add" type="text"/>
+							<input type="submit" class="add-tile__submit button is-primary" value="Add Tile">
 						</p>
 					</div>
-
-
-					<div class="field is-grouped is-pulled-right">
-						<p class="control">
-							<input type="submit" class="button is-primary" value="Add Tile">
-						</p>
-					</div>
-
 				</form>
+
 			</div>
 		</div>
-		<table class="table is-fullwidth">
-			<thead>
-				<tr v-for="(tile) in this.$store.getters.getTiles.slice(0,1)" v-bind:key="tile.id">
-					<th v-for="(value, name, index) in filteredTiles(tile)" v-bind:key="index">
-						{{name}}
-					</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr class="is-multiline" v-for="(tile) in this.$store.getters.getTiles" v-bind:key="tile.id">
-					<td v-for="(value, name, index) in filteredTiles(tile)" v-bind:key="index">
-						<input v-on:change="updataData($event, value, name, tile.id)" type="text" :value="value"/>
-					</td>
-		
-					<td><input type="button" value="delete" @click="deleteTile(tile.id)"/></td>
-					
-				</tr>
-			</tbody>
-		</table>
 	</div>
 </template>
 
@@ -62,9 +77,52 @@ export default {
 	data () {
 		return {
 			msg: 'Welcome to Your Vue.js App',
-			activeField: null,
+			newColumn: false,
+			newColumnText: '',
 			newTile: {
-				text: '',
+				title: '',
+			}
+		}
+	},
+	beforeCreate: function () {
+		this.$store.dispatch('setTiles');
+	},
+	computed: {
+		},
+	methods: {
+		filteredTiles: function(tile) {
+			const FilteredTile = {...tile};
+
+			delete FilteredTile.id;
+			delete FilteredTile.createdAt;
+			delete FilteredTile.title;
+
+			return FilteredTile;
+		},
+		updateTitle: function(event, tile){
+			this.updataData(event, 'title', tile.id);
+		},
+		updataData: function(event, key, id){
+			console.log(event, key, id)
+			db.collection("tiles").doc(id).update(`${key}`, event.target.value).catch((error) => {
+				console.error("Error updating document: ", error);
+			});
+		},
+		addColumn: async function(tile){
+			const snapshot = await db.collection('tiles').get()
+			snapshot.docs.map(doc => { 
+				doc.update(`${key}`, '').catch((error) => {
+					console.error("Error adding a new column: ", error);
+				});
+			});
+			
+		},
+		addTile: function(){
+			console.log('addTile')
+			const tile = this.newTile;
+
+			db.collection('tiles').add({
+				title: tile.title,
 				werkproces: '',
 				assignee: '',
 				status: '',
@@ -72,44 +130,9 @@ export default {
 				estimation: '',
 				timetracking: '',
 				deadline: '',
-			}
-		}
-	},
-	beforeCreate: function () {
-		this.$store.dispatch('setTiles');
-		this.$store.dispatch('setColumns');
-	},
-	filters: {
-
-	},
-	methods: {
-		filteredTiles: function(tile) {
-			const FilteredTile = {...tile};
-
-			delete FilteredTile.id;
-			delete FilteredTile.createdAt;
-
-			return FilteredTile;
-		},
-		updataData: function(event, tile, value, id){
-			db.collection("tiles").doc(id).update(`${value}`, event.target.value).catch((error) => {
-				console.error("Error updating document: ", error);
-			});
-		},
-		addTile: function(){
-			const tile = this.newTile;
-
-			db.collection('tiles').add({
-				text: tile.text,
-				werkproces: tile.werkproces,
-				assignee: tile.assignee,
-				status: tile.status,
-				timeline: tile.timeline,
-				estimation: tile.estimation,
-				timetracking: tile.timetracking,
-				deadline: tile.deadline,
 				createdAt: new Date(),
 			});
+			this.newTile.title = '';
 		},
 		deleteTile: function(id){
 			db.collection("tiles").doc(id).delete().catch((error) => {
@@ -123,9 +146,66 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss">
+	@import '~bulma';
 
 	$link: red !default;
-	h1 {
-		color: $link;
+	$height: 35px;
+
+	.dropdown {
+		&.is-active  {
+			.dropdown-icon {
+				transform: rotate(45deg);
+			}
+		}
+	}
+	.is-sticky {
+		position: sticky;
+		left: 0;
+		background: $light;
+	}
+	.table-wrapper {
+		overflow-x: auto;
+		&:-webkit-scrollbar { 
+            display: none; 
+        } 
+	}
+	.table {
+		min-width: 800px;
+		&:not(:last-child) {
+			margin-bottom: 0;
+		}
+		tbody {
+			tr {
+				height: $height;
+				td {
+					border: none;
+				}
+			}
+		}
+		input[type="text"] {
+			appearance: none;
+			border: none;
+			background: transparent;
+		}
+	}
+
+	.add-tile {
+		border: 1px solid #e6e9ef;
+		height: $height;
+		background: #fff;
+		outline: none;
+		padding-left: 20px;
+		width: 100%;
+		&__submit {
+			position: absolute;
+			top: 0;
+			right: 0;
+			border-radius: 0;
+			height: 100%;
+			opacity: 0;
+		}
+		&:focus + &__submit {
+			opacity: 1;
+		}
 	}
 </style>
